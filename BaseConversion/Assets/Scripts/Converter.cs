d2301b5d4ac6381cf19afc4f9fd846bb8d07a17c
KimberlyRaup <raupk@conwayschools.net>
2021-07-15 08:54:59 -0500

    Added Menus

--- BaseConversion/Assets/Scripts/Converter.cs
+++ BaseConversion/Assets/Scripts/Converter.cs
@@ -2,45 +2,38 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Converter : MonoBehaviour
 {
     public InputField valueInput = null;
-    public Dropdown options = null;
-    
+    public Dropdown valueBase = null;
     public Text binaryText = null;
     public Text octalText = null;
-    public Text decText = null;
-    public Text hexText = null;
-
+    public Text decimalText = null;
+    public Text hexidecimalText = null; 
+    
     string value = "";
     long numValue = 0;
 
     public void ConvertValue()
     {
         value = valueInput.text;
-        
-        if (value == "")
-        {
-            binaryText.text = "No value";
-        }
-        else 
-        {    
-           if(options.value == 0) { numValue = Convert.ToInt64(value, 2); }
-           else if (options.value == 1) { numValue = Convert.ToInt64(value, 8); }
-           else if (options.value == 2) { numValue = Convert.ToInt64(value, 10); }
-           else { numValue = Convert.ToInt64(value, 16); }
 
-           
+        if (value == "") 
+        {
+            binaryText.text = "No Value";
+        }
+        else
+        {
+            if (valueBase.value == 0) {numValue = Convert.ToInt64(value, 2);}
+            else if (valueBase.value == 1) {numValue = Convert.ToInt64(value, 8);}
+            else if (valueBase.value == 2) {numValue = Convert.ToInt64(value, 10);}
+            else if (valueBase.value == 3) {numValue = Convert.ToInt64(value, 16);}
+            
             binaryText.text = Convert.ToString(numValue, 2);
             octalText.text = Convert.ToString(numValue, 8);
-            decText.text = Convert.ToString(numValue, 10);
-            hexText.text = Convert.ToString(numValue, 16);
+            decimalText.text = Convert.ToString(numValue, 10);
+            hexidecimalText.text = Convert.ToString(numValue, 16);
 
-        } 
-
-
+        }
     }
-
-
 }
